537ab9dc9f0662e46319146f52fe271a8dd27daf
Alessandro1022 <wehelie98@gmail.com>
2026-03-20 04:10:51 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,9 +1,9 @@
 // src/integrations/supabase/client.ts
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-// Din Supabase URL och Publishable Key
-const supabaseUrl = "https://puztaocorkofidniafvu.supabase.co";
-const supabaseKey = "sb_publishable_RiXP2A9KBKtyUO5J1a-Vfw_sAVJ71Px";
+// Läs URL och Publishable Key från .env
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!;
 
 // Skapa Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey, {
