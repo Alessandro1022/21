ff2a4221892ba09308739401e4f7164c26bf5ade
Alessandro1022 <wehelie98@gmail.com>
2026-03-21 03:52:00 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,15 +1,20 @@
-// src/integrations/supabase/client.ts
 import { createClient } from "@supabase/supabase-js";
 
-// Läs URL och Publishable Key från .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!;
+const supabaseServiceKey = import.meta.env.VITE_SUPABASE_SERVICE_KEY!;
 
-// Skapa Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     storage: localStorage,
     persistSession: true,
     autoRefreshToken: true,
+  },
+});
+
+export const adminSupabase = createClient(supabaseUrl, supabaseServiceKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
   },
 });
