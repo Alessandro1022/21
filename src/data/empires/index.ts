86172fe06493b917ae751f56f17d9f0ebcdba321
Alessandro1022 <wehelie98@gmail.com>
2026-04-11 18:41:47 +0200

    Rename import for mongol empire to match convention

--- src/data/empires/index.ts
+++ src/data/empires/index.ts
@@ -1,17 +1,14 @@
 import { ottomanEmpire } from "./ottoman";
 import { romanEmpire } from "./roman";
 import { islamicEmpire } from "./islamic";
-import { mongol_empire } from "./mongol";
+import { mongolEmpire } from "./mongol";
 import type { EmpireConfig } from "./types";
-
 export type { EmpireConfig };
 export { formatYear } from "./types";
-
 export const empires: Record<string, EmpireConfig> = {
   ottoman: ottomanEmpire,
   roman: romanEmpire,
   islamic_caliphate: islamicEmpire,
-  mongol: mongol_empire,
+  mongol: mongolEmpire,
 };
-
 export const empireList: EmpireConfig[] = Object.values(empires);
