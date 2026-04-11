aa54922e0b26c80f79966e76bff60d710e8a0724
Alessandro1022 <wehelie98@gmail.com>
2026-04-11 18:37:17 +0200

    Add Mongol Empire to empires index

--- src/data/empires/index.ts
+++ src/data/empires/index.ts
@@ -1,6 +1,7 @@
 import { ottomanEmpire } from "./ottoman";
 import { romanEmpire } from "./roman";
 import { islamicEmpire } from "./islamic";
+import { mongol_empire } from "./mongol";
 import type { EmpireConfig } from "./types";
 
 export type { EmpireConfig };
@@ -10,6 +11,7 @@ export const empires: Record<string, EmpireConfig> = {
   ottoman: ottomanEmpire,
   roman: romanEmpire,
   islamic_caliphate: islamicEmpire,
+  mongol: mongol_empire,
 };
 
 export const empireList: EmpireConfig[] = Object.values(empires);
