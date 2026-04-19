16daa712b97c60f1c23857afd543b282c4e4872c
Alessandro1022 <wehelie98@gmail.com>
2026-04-19 20:20:39 +0200

    Add Documents route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 import { EmpireProvider } from "@/contexts/EmpireContext";
 
 import Home from "./pages/Home";
@@ -19,6 +20,7 @@ import ProfileDetail from "./pages/ProfileDetail";
 import Lineage from "./pages/Lineage";
 import StoryMode from "./pages/StoryMode";
 import EmpireSelect from "./pages/EmpireSelect";
+import Documents from "@/pages/Documents";
 import Settings from "./pages/Settings";
 import NotFound from "./pages/NotFound";
 import Admin from "./pages/Admin";
@@ -63,6 +65,8 @@ const App = () => {
               <Route path="/profiles/:id" element={<ProtectedRoute><ProfileDetail /></ProtectedRoute>} />
               <Route path="/lineage" element={<ProtectedRoute><Lineage /></ProtectedRoute>} />
               <Route path="/story" element={<ProtectedRoute><StoryMode /></ProtectedRoute>} />
+              <Route path="/documents" element={<Documents />} />
+
 
               <Route path="/settings" element={<ProtectedRoute skipEmpireCheck><Settings /></ProtectedRoute>} />
               <Route path="/admin" element={<ProtectedRoute skipEmpireCheck><Admin /></ProtectedRoute>} />
